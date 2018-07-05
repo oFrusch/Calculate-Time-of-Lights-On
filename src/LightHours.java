077f7893f6f7d25244c97764418f97b3ee9b6719
Owen Carpenter <owenc343@gmail.com>
2018-07-05 16:16:24 -0400

    Added comments and made code more succinct

--- src/LightHours.java
+++ src/LightHours.java
@@ -1,39 +1,36 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LightHours {
 
-    public static ArrayList<Integer> readInput(String input){
-        ArrayList<Integer> returnList = new ArrayList<>();
+
+    // Uses two hashmaps - The first one is meant to sort the inputs according to the keys, so that the processing of adding values to the second hashmap is more accurate.
+    // There is definitely a simpler way to do this.
+    public static HashMap<Integer, Integer> readInput(String input){
         HashMap<Integer, Integer> intSet = new HashMap<>();
         HashMap<Integer, Integer> holderSet = new HashMap<>();
         char[] intervals = input.toCharArray();
-        for(int i = 0; i < intervals.length; i+=2){
-            int convInt = Character.getNumericValue(intervals[i]), convInt2 = Character.getNumericValue(intervals[i+1]);
+        for(int i = 0; i < intervals.length; i+=2){ // Instantiate first HashMap so that I will have sorted key-value pairs
+            int convInt = Character.getNumericValue(intervals[i]), convInt2 = Character.getNumericValue(intervals[i+1]); // Convert array of characters to integer key-values
             holderSet.put(convInt, convInt2);
         }
-        int prevRangeBeg = 0, prevRangeEnd = 0;
+        int prevRangeBeg = 0, prevRangeEnd = 0; // Variables that hold the current range of values - Used to see if a new range needs to be created or if the current one can be expanded. 
         for(Map.Entry<Integer, Integer> entry : holderSet.entrySet()){
-            if(entry.getKey() < prevRangeEnd && entry.getValue() >= prevRangeEnd){
+            if(entry.getKey() < prevRangeEnd && entry.getValue() >= prevRangeEnd){ // Check if entry key is within the currently created range. If not, new range is created.
                 intSet.put(prevRangeBeg, entry.getValue());
                 prevRangeEnd = entry.getValue();
             }else {
-                intSet.put(entry.getKey(), entry.getValue());
+                intSet.put(entry.getKey(), entry.getValue()); // Creation of new range
                 prevRangeBeg = entry.getKey();
                 prevRangeEnd = entry.getValue();
             }
         }
         System.out.println(intSet);
-        for(Map.Entry<Integer, Integer> interval : intSet.entrySet()) {
-            returnList.add(interval.getKey());
-            returnList.add(interval.getValue());
-        }
-        return returnList;
+        return intSet;
     }
 
-    public static ArrayList<Integer> readInput(int[] input){
-        ArrayList<Integer> returnList = new ArrayList<>();
+    // Alternate version of function where an array of integers can be given instead of a string
+    public static HashMap<Integer, Integer> readInput(int[] input){
         HashMap<Integer, Integer> intSet = new HashMap<>();
         HashMap<Integer, Integer> holderSet = new HashMap<>();
         for(int i = 0; i < input.length; i+=2){
@@ -51,25 +48,21 @@ public class LightHours {
             }
         }
         System.out.println(intSet);
-        for(Map.Entry<Integer, Integer> interval : intSet.entrySet()) {
-            returnList.add(interval.getKey());
-            returnList.add(interval.getValue());
-        }
-        return returnList;
+        return intSet;
     }
 
-    public static int hoursOn(ArrayList<Integer> intervals){
-        int hoursOn = 0;
-        for(int i = 0; i < intervals.size()-1; i+=2){
-            hoursOn += intervals.get(i+1) - intervals.get(i);
+    // Computes the total time the lights were on
+    public static int hoursOn(HashMap<Integer, Integer> intervals){
+        int timeOn = 0;
+        for(Map.Entry<Integer, Integer> entry : intervals.entrySet()){
+            timeOn += entry.getValue() - entry.getKey();  // Total hours = time left (value) - time entered (key)
         }
-        return hoursOn;
+        return timeOn;
     }
 
     public static void main(String ... args){
-        String input = "";
-        ArrayList<Integer> intervals = readInput(input);
-        System.out.println(intervals);
+        String input = "24361368";
+        HashMap<Integer, Integer> intervals = readInput(input);
         int totalHours = hoursOn(intervals);
         System.out.println(totalHours);
     }
